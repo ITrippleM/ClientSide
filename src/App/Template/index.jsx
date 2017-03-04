eb2e457cec286b80478d113e1c42bab645aabf9f
bearium <jacob_martin@rogers.com>
2017-03-04 18:03:45 -0500

    Fix imports

--- src/App/Template/index.jsx
+++ src/App/Template/index.jsx
@@ -2,7 +2,7 @@
  * Created by macdja38 on 2017-03-04.
  */
 
-import React, {Component} from 'react';
+import React, {Component, Link} from 'react';
 
 export default class Template extends Component {
   constructor(props) {
@@ -13,22 +13,22 @@ export default class Template extends Component {
   }
 
   render() {
-    return (<div>
-      <header>
-        <ul role="nav">
-          <li><Link to="about">About</Link></li>
-          <li><Link to="contact">Contact</Link></li>
-        </ul>
-      </header>
-      <main>
-        // this is like doing <About/> or <Contact/>
-        // but the router does it for you
-        {this.props.activeRoute}
-      </main>
-      <footer>
-        Copyright 2014 Somebody
-      </footer>
-    </div>
-  )
+    console.log(this.props);
+    return (
+      <div>
+        <header>
+          <ul role="nav">
+            <li><Link to="about">About</Link></li>
+            <li><Link to="contact">Contact</Link></li>
+          </ul>
+        </header>
+        <main>
+          {this.props.activeRoute}
+        </main>
+        <footer>
+          Copyright 2014 Somebody
+        </footer>
+      </div>
+    )
   }
 }
